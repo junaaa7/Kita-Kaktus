fd39438bb1b4f0da432843adcdaba02ec9e513cc
DODGE-bit <arjunaputra1507@gmail.com>
2026-06-05 03:41:55 +0700

    Add Google Search Console verification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="d_hYjvoz1418rL43JJhJhrkybLY1O9DQBNhgOFyB720" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Kita Kaktus')</title>
     
